e6055e97d35b23dabe5026f71563b7f557a1586d
maisha02 <maishamehedi@gmail.com>
2026-03-23 14:16:18 +1100

    rn-navigation.md
    Navigation in React Native using React Navigation (Closes #27)

--- rn-gestures-app/app/(tabs)/index.tsx
+++ rn-gestures-app/app/(tabs)/index.tsx
@@ -1,32 +1,17 @@
-import React, { useRef } from 'react';
-import { View, Button, Animated } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
-  const moveAnim = useRef(new Animated.Value(0)).current;
-
-  const startAnimation = () => {
-    moveAnim.setValue(0);
-
-    Animated.timing(moveAnim, {
-      toValue: 150,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  };
+  const router = useRouter();
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Animated.View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'blue',
-          marginBottom: 20,
-          transform: [{ translateX: moveAnim }],
-        }}
-      />
+      <Text>Home Screen</Text>
 
-      <Button title="Start Animation" onPress={startAnimation} />
+      <Button
+        title="Go to Explore"
+        onPress={() => router.push('/explore')}
+      />
     </View>
   );
 }
