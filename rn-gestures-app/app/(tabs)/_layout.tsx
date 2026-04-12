38d570a663c45fbf5ebecf0c041ac2e811781074
maisha02 <maishamehedi@gmail.com>
2026-04-13 00:33:10 +1000

    Create rn-logging.md
    Logging and Crash Reporting with Sentry (Closes #18)

--- rn-gestures-app/app/(tabs)/_layout.tsx
+++ rn-gestures-app/app/(tabs)/_layout.tsx
@@ -1,35 +1,11 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Stack } from 'expo-router';
+import * as Sentry from '@sentry/react-native';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+Sentry.init({
+  dsn: 'https://35dc8d477a7ccac738d0b31b2ef1becb@o4511136914407424.ingest.us.sentry.io/4511136925483008',
+  debug: true,
+});
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
