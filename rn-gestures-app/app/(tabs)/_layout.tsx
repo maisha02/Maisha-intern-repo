9bb3ebaf2928623926f8e0b358e9782c27c45b89
maisha02 <maishamehedi@gmail.com>
2026-04-13 01:07:26 +1000

    Create rn-deep-linking.md
    Handling Deep Linking and Routing (Closes #19)

--- rn-gestures-app/app/(tabs)/_layout.tsx
+++ rn-gestures-app/app/(tabs)/_layout.tsx
@@ -1,11 +1,21 @@
-import { Stack } from 'expo-router';
-import * as Sentry from '@sentry/react-native';
+import { Tabs } from 'expo-router';
+import React from 'react';
 
-Sentry.init({
-  dsn: 'https://35dc8d477a7ccac738d0b31b2ef1becb@o4511136914407424.ingest.us.sentry.io/4511136925483008',
-  debug: true,
-});
-
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+        }}
+      />
+    </Tabs>
+  );
 }
