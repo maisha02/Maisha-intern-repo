96ab0c714e13a0ff896f689e35d80c2f6d2c1eaa
maisha02 <maishamehedi@gmail.com>
2026-04-13 08:54:07 +1000

    Create rn-debugging.md
    Debugging React Native Apps (Closes #25)

--- rn-gestures-app/app/(tabs)/index.tsx
+++ rn-gestures-app/app/(tabs)/index.tsx
@@ -1,53 +1,26 @@
-import { View, Text, ActivityIndicator, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { useEffect, useState } from 'react';
-import api from '../api';
-
-type Post = {
-  title: string;
-  body: string;
-};
 
 export default function HomeScreen() {
-  const [data, setData] = useState<Post | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  const fetchData = async () => {
-    setLoading(true);
-    setError('');
-
-    try {
-      const response = await api.get('/posts/1');
-      setData(response.data);
-    } catch (err) {
-      setError('Failed to load data');
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [title, setTitle] = useState("Loading...");
 
   useEffect(() => {
-    fetchData();
+    console.log('App is running');
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('API response:', data);
+        setTitle(data.title);
+      })
+      .catch((err) => {
+        console.log('API error:', err);
+      });
   }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 20, backgroundColor: 'black' }}>
-      {loading && <ActivityIndicator size="large" />}
-  
-      {error !== '' && <Text style={{ color: 'white', marginBottom: 10 }}>{error}</Text>}
-  
-      {data && (
-        <>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white', marginBottom: 10 }}>
-            {data.title}
-          </Text>
-          <Text style={{ color: 'white', marginBottom: 20 }}>
-            {data.body}
-          </Text>
-        </>
-      )}
-  
-      <Button title="Reload" onPress={fetchData} />
+    <View style={{ padding: 70 }}>
+      <Text>Debug Test Screen</Text>
     </View>
   );
-      }
+}
